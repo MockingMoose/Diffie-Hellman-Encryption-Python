83ab420d3b62e496049b34a439f37bfaccaebdf3
MockingMoose <198123013+MockingMoose@users.noreply.github.com>
2026-05-19 23:22:02 -0400

    cleaned up to make more readable

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import streamlit as st
-import math 
 import random 
 
 st.title("Diffie-Hellman Visualization")
-primesList = [11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+
+## g = base, p = prime
+## a = Alice private key, b = Bob private key
+
+##Alice public key: A = g^a mod p
+##Bob public key: B = g^b mod p
+
+
 #Public Prime and Base
+primesList = [11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
 pubPrime = random.choice(primesList)
 pubBase = 3
 
 #Private Keys
-sendKey = 4
-receiveKey = 6
+aliceKey = 4
+bobKey = 6
+
 
 ##Generates a public key 
-def generate_public_key(base, prime, secret):
-    return pow(base, secret, prime)
+def generate_public_key(base, prime, private):
+    #Formula: (base ^ private) % prime
+    return pow(base, private, prime)
 
 ##Generates shared secret key
-def generate_shared_key(public, prime, secret):
-    return pow(public, secret, prime)
+def generate_shared_key(public, prime, private):
+    #Formula: (public ^ private) % prime
+    #Each person uses their own private key in combination with
+    #  the others public key to arrive at the same number
+    return pow(public, private, prime)
 
 
 ##Takes in a message and encrypts it with Caesar Cyper using the shared key
@@ -26,6 +38,7 @@ def encrypt(message, key):
     encryptMessage = ''
     for i in range(len(message)):
         charNum = ord(message[i])
+        #Shifts character by the value of the shared key
         encryptMessage += (chr(charNum + key))
     return encryptMessage
 
@@ -34,17 +47,18 @@ def decrypt(message, key):
     decryptMessage = ''
     for i in range(len(message)):
         charNum = ord(message[i])
+        #Shifts character by the value of the shared key
         decryptMessage += (chr(charNum - key))
     return decryptMessage
 
 
 #Public Keys
-sendPub = generate_public_key(pubBase, pubPrime, sendKey)
-receivePub = generate_public_key(pubBase, pubPrime, receiveKey)
+alicePub = generate_public_key(pubBase, pubPrime, aliceKey)
+bobPub = generate_public_key(pubBase, pubPrime, bobKey)
 
 #Shared Keys
-sendShared = generate_shared_key(receivePub, pubPrime, sendKey)
-receiveShared = generate_shared_key(sendPub, pubPrime, receiveKey)
+aliceShared = generate_shared_key(bobPub, pubPrime, aliceKey)
+bobShared = generate_shared_key(alicePub, pubPrime, bobKey)
 
 
 
@@ -52,29 +66,28 @@ receiveShared = generate_shared_key(sendPub, pubPrime, receiveKey)
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.header("Sender A (Alice)")
-    st.write(f"Private Key: {sendKey}")
-    st.write(f"Calculated Public Key: {sendPub}")
+    st.header("Sender (Alice)")
+    st.write(f"Private Key: {aliceKey}")
+    st.write(f"Calculated Public Key: {alicePub}")
 with col2:
     st.header("The Public Space")
     st.write(f"Shared Prime (p): {pubPrime}")
     st.write(f"Shared Base (g): {pubBase}")
 with col3:
-    st.header("Receiver B (Bob)")
-    st.write(f"Private Key: {receiveKey}")
-    st.write(f"Calculated Public Key: {receivePub}")
+    st.header("Receiver (Bob)")
+    st.write(f"Private Key: {bobKey}")
+    st.write(f"Calculated Public Key: {bobPub}")
 st.divider()
 
-st.subheader("Sender A sends a message encrypted with shared key")
-secretMessage = encrypt(st.text_input("Enter your message to be encrypted"), sendShared)
+st.subheader("Alice sends a message encrypted with shared key")
+secretMessage = encrypt(st.text_input("Enter your message to be encrypted"), aliceShared)
 st.divider()
 
 st.subheader("Intercepted Data on the Network")
 st.write("What the hacker sees:")
 st.write(f"Intercepted Message: {secretMessage}")
-st.write(f"Intercepted Public Keys: A={sendPub}, B= {receivePub}")
-
+st.write(f"Intercepted Public Keys: Alice={alicePub}, Bob= {bobPub}")
 st.divider()
 
-st.subheader("Receiver B gets message and decrypts it using shared key")
-st.success(f"Decrypted successfully by Receiver B: {decrypt(secretMessage, receiveShared)}")
+st.subheader("Receiver Bob gets message and decrypts it using shared key")
+st.success(f"Decrypted successfully by Bob: {decrypt(secretMessage, bobShared)}")
