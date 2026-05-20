9aeaf55b2c3b1f01a069a6ea72f1750cb6ee2d70
MockingMoose <198123013+MockingMoose@users.noreply.github.com>
2026-05-19 20:59:02 -0400

    added keys and generate functions

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import streamlit as st
-import math as math
-
+import math 
+import random 
 
 st.title("Diffie-Hellman Visualization")
 
+pubPrime = 17
+pubBase = 3
+sendKey = 4
+receiveKey = 15
+sendPub = generate_public_key(pubBase, pubPrime, sendKey)
+receivePub = generate_public_key(pubBase, pubPrime, receiveKey)
 
-secret = "Hello"
+##Generates a public key 
+def generate_public_key(base, prime, secret):
+    return pow(base, secret, prime)
+
+##Generates shared secret key
+def generate_shared_key(public, prime, secret):
+    return pow(public, secret, prime)
+
 
 ##Takes in a message and encrypts it with Caesar Cyper using the shared key
 def encrypt(message, key):
@@ -15,8 +28,6 @@ def encrypt(message, key):
         encryptMessage += (chr(charNum + key))
     return encryptMessage
 
-
-
 ##Takes in an encrypted message and decrypts using Caesar Cyper using the shared key
 def decrypt(message, key):
     decryptMessage = ''
@@ -25,5 +36,5 @@ def decrypt(message, key):
         decryptMessage += (chr(charNum - key))
     return decryptMessage
 
-print(encrypt(secret, 5))
-print(decrypt(encrypt(secret, 5), 5))
+print(generate_public_key(pubBase, pubPrime, sendKey))
+print(generate_shared_key(pubBase, pubPrime, receiveKey))
